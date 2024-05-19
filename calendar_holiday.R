cba1903e73477b90bb963cea8dcecaa88d2a00c1
Dr Minion <74802168+Mr-Minion@users.noreply.github.com>
2024-05-19 19:15:41 +0200

    Update calendar_holiday.R

--- calendar_holiday.R
+++ calendar_holiday.R
@@ -3,7 +3,7 @@
 ##############################
 
 # 1. Description------
-# This script pulls, sorts and modifies holiday calendar data over multiple years in an Egnlish and German version.
+# This script pulls, sorts and modifies holiday calendar data over multiple years in an English and German version.
 # The output are automatically generated as an English and German Excel file.
 # Import into PowerBI is possible as well.
 
